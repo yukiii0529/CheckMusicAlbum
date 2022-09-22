f79ed2fca082842a8839d32fbadd0c4c6bd93be8
yukiii0529 <57751716+yukiii0529@users.noreply.github.com>
2022-09-23 01:53:58 +0900

    Initial Commit

---
+++ CheckMusicAlbum/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CheckMusicAlbum
+//
+//  Created by 田中勇輝 on 2022/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
